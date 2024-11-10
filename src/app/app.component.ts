934c6ac6bc05d9fb89a0b84e5dcb47e8b9c2d533
griffone <brunogriffone15@gmail.com>
2024-11-10 20:39:24 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { createSwapy } from 'swapy';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend-bento';
+export class AppComponent implements AfterViewInit {
+  title = 'Portfolio';
+  swapy: any;
+
+  ngAfterViewInit() {
+    const container = document.querySelector('.container');
+    this.swapy = createSwapy(container, {
+      animation: 'dynamic'
+    });
+    this.swapy.enable(true);
+  }
 }
