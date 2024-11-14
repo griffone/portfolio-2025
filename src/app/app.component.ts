db06e5576b7e130118186f21b5ce786a54290dfb
griffone <brunogriffone15@gmail.com>
2024-11-14 14:39:11 -0300

    devicon config - set the hover effect properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+
+import { diAngularOriginal, diSpringOriginal } from '@ng-icons/devicon/original';
 import { createSwapy } from 'swapy';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIconComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  viewProviders: [provideIcons({diSpringOriginal, diAngularOriginal})]
 })
 export class AppComponent implements AfterViewInit {
   title = 'Portfolio';
